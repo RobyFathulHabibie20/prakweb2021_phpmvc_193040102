1c17d89509ae034baedda774e3e58085d422b285
RobyFathulHabibie20 <63381738+RobyFathulHabibie20@users.noreply.github.com>
2021-10-01 17:57:15 +0700

    Menyelesaikan vidio ke-6 mengenai Assets

--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class = mt-4>About Me</h1>
+    <img src="<?= BASEURL; ?>/img/ts.jfif" alt="Thomas Shelby" width = 200 class = "rounded-circle shadow">
     <p>Hallo, Nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> tahun, Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
